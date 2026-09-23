875945a2d000587dbd34610ee330979dc6afe41e
sahilrana1940 <sahilrana1940@gmail.com>
2026-09-24 02:45:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,10 @@
 from flask import Flask, render_template, send_from_directory
-import os
 app = Flask(__name__)
 
 @app.route('/.well-known/assetlinks.json')
 def assetlinks():
-    return send_from_directory(
-        os.path.join(app.root_path, '.well-known'), 
-        'assetlinks.json',
-        mimetype='application/json'
-    )
+    return send_from_directory('.well-known', 'assetlinks.json')
 
 @app.route('/')
 def home():
-    return render_template('index.html')
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    ... baki tera code ...
